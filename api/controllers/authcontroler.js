7d933d2e573543d7e81a6d18ece56e27047d2fcc
Md Ashraful alam <ashrafulalam10000@gmail.com>
2024-07-06 22:30:57 +0600

    updated mode

--- api/controllers/authcontroler.js
+++ api/controllers/authcontroler.js
@@ -237,7 +237,7 @@ export const userLogin = asyncHandler(async (req, res) => {
     res.cookie('accessToken', token, {
       httpOnly: true,
       secure: process.env.APP_ENV !== 'development'?false:true,
-      sameSite: 'strict',
+      sameSite: 'Lax',
       maxAge: 1000 * 60 * 60 * 24 * 7,
     });
 
